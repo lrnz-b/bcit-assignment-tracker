52e5e213e8e7027b468f3f8bd7f9b90756dbeb79
lrnz-b <lbarnill@my.bcit.ca>
2023-05-09 21:01:48 -0700

    disable/enable button based on input value

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
+import { useState } from "react";
 
 export function Header() {
+  const [newAssignmentInput, setNewAssignmentInput] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewAssignmentInput(event.currentTarget.value);
+  }
+
   return (
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
-        <button>
+        <input placeholder="Add a new assignment" type="text" onChange={handleChange} />
+        <button disabled={!newAssignmentInput} >
           Create <AiOutlinePlusCircle size={20} />
         </button>
       </form>
